23830d4404828b0719f752d03b339225d123865a
Cam Remesz <saltyndead@gmail.com>
2022-03-04 16:54:44 -0700

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './pages/map/map.component';
+import { RestaurantComponent } from './pages/restaurant/restaurant.component';
+import { RestaurantDetailsComponent } from './pages/restaurant/restaurant-details/restaurant-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: MapComponent },
+  { path: 'restaurant', component: RestaurantComponent},
+  { path: 'restaurant/:id', component: RestaurantDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,10 @@ export class AppComponent {
 
   title = 'wwjd';
 
-  // Get
-  mainUrl = 'https://api.yelp.com/v3/businesses/search/restaurants'
+  // Get (5000 per day)
+  mainUrl = 'https://api.yelp.com/v3/businesses/search/restaurants';
+
+  clientId = '1iFntpQryaxCWpLl1Y_DGQ';
+
+  apiKey = 'WVpTr5z0iyGLH-7mkFeZDwHaM1FHDhd1dXR1ZOCgoxRU5dbmrNHz0zQP0SIDMfwdsz5qN1mOuLAykBvncTOp1xSI4n1waEloijfmMUcCFO_3rIxBBDtuGKA0JJIiYnYx';
 }
