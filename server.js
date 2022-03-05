c38b1ea46ba8a7a9d07c1d78528adcbb48cb6c6c
Cam Remesz <saltyndead@gmail.com>
2022-03-05 09:08:59 -0700

    Set up proxy server for api call

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express'); 
+const request = require('request'); 
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:4200'
+}))
+
+
+app.get('/api', function(req, res){ 
+  request('https://api.yelp.com/v3/businesses/search?term=restaurants&location=calgary', function (error, response, body) { 
+    if (!error && response.statusCode === 200) { 
+      console.log(body); 
+      res.send(body); 
+    }
+    if(error) {
+      console.log(error);
+    } 
+   })
+   .auth(null, null, true, 'WVpTr5z0iyGLH-7mkFeZDwHaM1FHDhd1dXR1ZOCgoxRU5dbmrNHz0zQP0SIDMfwdsz5qN1mOuLAykBvncTOp1xSI4n1waEloijfmMUcCFO_3rIxBBDtuGKA0JJIiYnYx');
+});
+
+
+
+app.listen(8080);
+console.log('Server running on port 8080...');
